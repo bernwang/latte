173931ea54ab47082895f8a19a075d01d83f2c72
Alvin Wan <hi@alvinwan.com>
2017-08-15 18:01:46 -0700

    finished basic 3d annotation

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# Antsy
+# Antsy 3D
 Lightweight point cloud annotation tool for instance-level segmentation using fat markers and raytracing.
+
+> Sorry, current version using skinny (1-pixel) marker. :( Another option is to use Three.js's built-in octotree and color entire boxes.
 
 This is an in-browser interactive view of your models. To run, use the following
 from the root of this repository. Navigate to the repository root. Double-click on `index.html` to launch the app. No installation is required.
+
+# Point Cloud Format
+
+`js/output.js` defines a global variable `data`, a dictionary mapping unique point cloud names to point cloud data; point clouds have a `vertices` property, a list of vertices, each with `.x`, `.y` and `.z` properties.
+
+e.g., To access the `x` position of the first point in the point cloud named `frog`, you would access `data.frog.vertices[0].x`.
--- js/main.js
+++ js/main.js
@@ -1,92 +1,217 @@
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
 var renderer, scene, camera, stats;
+var pointcloud;
+var raycaster;
+var mouse = new THREE.Vector2();
+var intersection = null;
+var spheres = [];
+var sphereXs = new Set();
+var clock;
 
-var particles1, particles2, uniforms, grid;
-
-var PARTICLE_SIZE = 10;
+var threshold = 0.1;
+var pointSize = 0.5;
 
 init();
 animate();
 
-function initGrid(scene) {
-  grid = new THREE.GridHelper( 200, 20 );
-  grid.setColors( 0xffffff, 0xffffff );
-  scene.add( grid );
+// Should be in init?
+var sphereGeometry = new THREE.SphereGeometry( 0.1, 32, 32 );
+var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, shading: THREE.FlatShading } );
+
+function generatePointCloudGeometry( vertices, color ){
+
+    var geometry = new THREE.BufferGeometry();
+
+    var positions = new Float32Array( vertices.length*3 );
+    var colors = new Float32Array( vertices.length*3 );
+
+    var k = 0;
+
+    for ( var i = 0, l = vertices.length; i < l; i ++ ) {
+
+        vertex = vertices[ i ];
+        positions[ 3 * k ] = vertex.x;
+        positions[ 3 * k + 1 ] = vertex.y;
+        positions[ 3 * k + 2 ] = vertex.z;
+
+        var intensity = ( vertex.y + 0.1 ) * 5;
+        colors[ 3 * k ] = color.r * intensity;
+        colors[ 3 * k + 1 ] = color.g * intensity;
+        colors[ 3 * k + 2 ] = color.b * intensity;
+
+        k++;
+    }
+
+    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+    geometry.computeBoundingBox();
+
+    return geometry;
+
 }
 
-function initParticlesForCluster(obj_name, offset) {
-  return initParticlesForObject(data[obj_name]['vertices'], offset, 1);
-}
 
-function initParticlesForObject(vertices, offset, multiplier) {
+function generatePointCloud( vertices, color ) {
 
-  var positions = new Float32Array( vertices.length * 3 );
-  var colors = new Float32Array( vertices.length * 3 );
-  var sizes = new Float32Array( vertices.length );
+    var geometry = new THREE.Geometry();
 
-  var vertex;
-  var color = new THREE.Color();
+    var colors = [];
 
-  for ( var i = 0, l = vertices.length; i < l; i ++ ) {
+    var k = 0;
 
-    vertex = vertices[ i ];
-    positions[i * 3] = vertex.x * multiplier;
-    positions[i * 3 + 1] = vertex.z * multiplier;
-    positions[i * 3 + 2] = vertex.y * multiplier;
+    for ( var i = 0, l = vertices.length; i < l; i ++ ) {
 
-    color.setHSL( offset + 0.1 * ( i / l ), 1.0, 0.5 );
-    color.toArray( colors, i * 3 );
+        vertex = vertices[ i ];
+        var v = new THREE.Vector3( vertex.x, vertex.y, vertex.z );
+        geometry.vertices.push( v );
 
-    sizes[ i ] = PARTICLE_SIZE * 0.5;
+        var intensity = ( vertex.y + 0.1 ) * 7;
+        colors[ k ] = ( color.clone().multiplyScalar( intensity ) );
 
-  }
+        k++;
+    }
 
-  var geometry = new THREE.BufferGeometry();
-  geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-  geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
-  geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
+    geometry.colors = colors;
+    geometry.computeBoundingBox();
 
-  var material = new THREE.ShaderMaterial( {
+    var material = new THREE.PointsMaterial( { size: pointSize, vertexColors: THREE.VertexColors } );
+    var pointcloud = new THREE.Points( geometry, material );
 
-    uniforms: {
-      color:   { value: new THREE.Color( 0xffffff ) },
-      texture: { value: new THREE.TextureLoader().load( "textures/sprites/disc.png" ) }
-    },
-    vertexShader: document.getElementById( 'vertexshader' ).textContent,
-    fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+    return pointcloud;
 
-    alphaTest: 0.9
-
-  } );
-
-
-  var particles = new THREE.Points( geometry, material );
-  return particles;
 }
 
 function init() {
 
-  container = document.getElementById( 'container' );
+    var container = document.getElementById( 'container' );
 
-  scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.y = 50;
-  camera.position.z = 75;
+    clock = new THREE.Clock();
 
-  initNav();
-  initGrid(scene);
-  show(document.getElementById('obj0'), Object.keys(data)[0]);
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.y = 50;
+    camera.position.z = 75;
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  container.appendChild( renderer.domElement );
+    //
 
-  controls = new THREE.OrbitControls( camera, renderer.domElement );
+    var sphereGeometry = new THREE.SphereGeometry( 0.1, 32, 32 );
+    var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, shading: THREE.FlatShading } );
 
-  window.addEventListener( 'resize', onWindowResize, false );
+//    for ( var i = 0; i < 40; i++ ) {
+//
+//        var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+//        scene.add( sphere );
+//        spheres.push( sphere );
+//
+//    }
+
+    //
+
+    var grid = new THREE.GridHelper( 200, 20 );
+    grid.setColors( 0xffffff, 0xffffff );
+    scene.add( grid );
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    //
+
+    raycaster = new THREE.Raycaster();
+    raycaster.params.Points.threshold = threshold;
+
+    //
+
+    stats = new Stats();
+    container.appendChild( stats.dom );
+
+    //
+
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize, false );
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+    //
+
+    initNav();
+    show(document.getElementById('obj0'), Object.keys(data)[0]);
 
 }
+
+function onDocumentMouseMove( event ) {
+
+    event.preventDefault();
+
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    render();
+    stats.update();
+
+}
+
+var toggle = 0;
+
+function render() {
+
+    raycaster.setFromCamera( mouse, camera );
+
+    var intersections = raycaster.intersectObjects( pointcloud );
+    intersection = ( intersections.length ) > 0 ? intersections[ 0 ] : null;
+
+    if ( toggle > 0.005 && intersection !== null && !(intersection.point.x in sphereXs)) {
+
+        sphereXs.add(intersection.point.x);
+        var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+        sphere.position.copy( intersection.point );
+        scene.add(sphere);
+        spheres.push(sphere);
+//        spheres[ spheresIndex ].position.copy( intersection.point );
+//        spheres[ spheresIndex ].scale.set( 1, 1, 1 );
+//        spheresIndex = ( spheresIndex + 1 ) % spheres.length;
+
+        toggle = 0;
+
+    }
+
+//    for ( var i = 0; i < spheres.length; i++ ) {
+//
+//        var sphere = spheres[ i ];
+//        sphere.scale.multiplyScalar( 0.98 );
+//        sphere.scale.clampScalar( 0.01, 1 );
+//
+//    }
+
+    toggle += clock.getDelta();
+
+    renderer.render( scene, camera );
+
+}
+
+// Navigation Bar
 
 function initNav() {
     navigation = document.getElementById('navigation');
@@ -115,40 +240,18 @@ function show(button, obj_name) {
     button.className += "selected";
     var rotation = 0;
 
-    if (particles1 !== undefined) {
-        scene.remove(particles1);
-        rotation = particles1.rotation.y;
+    if (pointcloud !== undefined) {
+        scene.remove(pointcloud);
+        rotation = pointcloud.rotation.y;
     }
 
-    particles1 = initParticlesForCluster(obj_name, 0.5);
-    particles1.rotation.y = rotation;
+    var pcRegular = generatePointCloudForCluster( obj_name );
+    pcRegular.rotation.y = rotation;
+    scene.add( pcRegular );
 
-    scene.add(particles1);
+    pointcloud = [pcRegular];
 }
 
-function onWindowResize() {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-function animate() {
-
-  requestAnimationFrame( animate );
-
-  render();
-
-}
-
-function render() {
-
-  rate = 0
-  particles1.rotation.y += rate;
-  grid.rotation.y += rate;
-
-  renderer.render( scene, camera );
-
+function generatePointCloudForCluster(obj_name) {
+  return generatePointCloud(data[obj_name]['vertices'], new THREE.Color( 1,0,1 ));
 }
