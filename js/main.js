1e29a85d33b56189751001dbb4aa0fc32aa9d0d2
bernie wang <berniewang@berkeley.edu>
2018-03-19 15:38:07 -0700

    added bounding box drawing capabilities

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var renderer, scene, camera, stats;
@@ -5,52 +7,40 @@ var pointcloud;
 var raycaster;
 var mouse = new THREE.Vector2();
 var intersection = null;
-var spheres = [];
-var pointsWithSpheres = new Set();
 var clock;
 var mouseDown;
 var highlightMode = false;
-
 var threshold = 0.1;
 var pointSize = 0.5;
+// data structures
+var data;
+var spheres = [];
+var pointsWithSpheres = new Set();
+var selectedPoints = [];
+var boxMap = new Map();
+var boundingBoxes = [];
+var image_loaded = false;
 
+var newBoundingBox;
+var newBox;
+
+var mouse = new THREE.Vector3();
+var anchor = new THREE.Vector3();
+
+var boxgeometry = new THREE.BoxGeometry( 1, 1, 1 );
+var boxmaterial = new THREE.MeshDepthMaterial( {opacity: .1} );
+var move2D = false;
+
+var t = 0, dt = 0.01;                   // t (dt delta for demo)
+var start, end;
+    // a = {x: -2, y: -1, z: -1},          // start position
+    // b = {x: 1.5, y: 0.5, z: 0.7};       // end position
+    
 init();
-animate();
+// animate();
 
 // Should be in init?
 var sphereGeometry, sphereMaterial;
-
-function generatePointCloudGeometry( vertices, color ){
-
-    var geometry = new THREE.BufferGeometry();
-
-    var positions = new Float32Array( vertices.length*3 );
-    var colors = new Float32Array( vertices.length*3 );
-
-    var k = 0;
-
-    for ( var i = 0, l = vertices.length; i < l; i ++ ) {
-
-        vertex = vertices[ i ];
-        positions[ 3 * k ] = vertex.x;
-        positions[ 3 * k + 1 ] = vertex.y;
-        positions[ 3 * k + 2 ] = vertex.z;
-
-        var intensity = ( vertex.y + 0.1 ) * 5;
-        colors[ 3 * k ] = color.r * intensity;
-        colors[ 3 * k + 1 ] = color.g * intensity;
-        colors[ 3 * k + 2 ] = color.b * intensity;
-
-        k++;
-    }
-
-    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
-    geometry.computeBoundingBox();
-
-    return geometry;
-
-}
 
 
 function generatePointCloud( vertices, color ) {
@@ -62,12 +52,14 @@ function generatePointCloud( vertices, color ) {
     var k = 0;
 
     for ( var i = 0, l = vertices.length; i < l; i ++ ) {
-
-        vertex = vertices[ i ];
-        var v = new THREE.Vector3( vertex.x, vertex.y, vertex.z );
+        // creates new vector from a cluster and adds to geometry
+        var v = new THREE.Vector3( vertices[ 32 * k + 1 ], 
+            vertices[ 32 * k + 2 ], vertices[ 32 * k ] );
+        // var v = new THREE.Vector3( vertices[ 32 * k + 1 ], 
+        //     0, vertices[ 32 * k ] );
         geometry.vertices.push( v );
 
-        var intensity = ( vertex.y + 0.1 ) * 7;
+        var intensity = ( 1 ) * 7;
         colors[ k ] = ( color.clone().multiplyScalar( intensity ) );
 
         k++;
@@ -77,12 +69,14 @@ function generatePointCloud( vertices, color ) {
     geometry.computeBoundingBox();
 
     var material = new THREE.PointsMaterial( { size: pointSize, vertexColors: THREE.VertexColors } );
+    // creates pointcloud given vectors
     var pointcloud = new THREE.Points( geometry, material );
 
     return pointcloud;
 
 }
 
+// called first, populates scene and initializes renderer
 function init() {
 
     var container = document.getElementById( 'container' );
@@ -92,10 +86,11 @@ function init() {
     clock = new THREE.Clock();
 
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.y = 25;
-    camera.position.z = 37;
+    // camera.position.y = 25;
+    // camera.position.z = 37;
+    camera.position.set(0, 100, 0);
+    camera.lookAt(new THREE.Vector3(0,0,0));
 
-    //
 
     sphereGeometry = new THREE.SphereGeometry( 0.1, 32, 32 );
     sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, shading: THREE.FlatShading } );
@@ -126,8 +121,11 @@ function init() {
     //
 
     controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.enabled = false;
-
+    // controls.enabled = false;
+    // controls.maxPolarAngle = 0;
+    // controls.minPolarAngle = 0;
+    // controls.maxAzimuthAngle = Math.PI;
+    // controls.minAzimuthAngle = -Math.PI;
     //
 
     window.addEventListener( 'resize', onWindowResize, false );
@@ -137,36 +135,74 @@ function init() {
     document.getElementById( 'save' ).addEventListener( 'click', save, false );
     document.getElementById( 'export' ).addEventListener( 'click', save_image, false );
     document.getElementById( 'move' ).addEventListener( 'click', moveMode, false );
+    document.getElementById( 'move2D' ).addEventListener( 'click', move2DMode, false );
     document.getElementById( 'label' ).addEventListener( 'click', labelMode, false );
+    document.getElementById( 'file_input' ).addEventListener( 'change', upload_file, false );
+    document.getElementById( 'draw-box').addEventListener( 'click', drawBox, false );
+}
 
-    //
+function getMin(v1, v2) {
+    return new THREE.Vector3(Math.min(v1.x, v2.x), 
+                             Math.min(v1.y, v2.y), 
+                             Math.min(v1.z, v2.z))
+}
 
-    initNav();
-    show(document.getElementById('obj0'), Object.keys(data)[0]);
-
+function getMax(v1, v2) {
+    return new THREE.Vector3(Math.max(v1.x, v2.x), 
+                             Math.max(v1.y, v2.y), 
+                             Math.max(v1.z, v2.z))
 }
 
 function onDocumentMouseMove( event ) {
 
     event.preventDefault();
-
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
+    if (mouseDown == true) {
+        var cursor = get3DCoord();
+        if (cursor.x != anchor.x && cursor.y != anchor.y && cursor.z != anchor.z) {
+            if (newBox != null) {
+                scene.add( newBox );
+            }
+            var minVector = getMin(cursor, anchor);
+            var maxVector = getMax(cursor, anchor);
+            newBoundingBox.set(minVector, maxVector);
+        }
+        
+    }
 }
 
 function onDocumentMouseUp( event ) {
 
     event.preventDefault();
     mouseDown = false;
-
+    newBox = null;
 }
 
 function onDocumentMouseDown( event ) {
 
     event.preventDefault();
-    mouseDown = true;
+    if (!controls.enabled) {
+        mouseDown = true;
+        anchor = get3DCoord();
+        var v = anchor.clone();
+        anchor.x += .000001;
+        anchor.y -= .000001;
+        anchor.z += .000001;
+        
+        newBoundingBox = new THREE.Box3(anchor, v);
+        anchor = anchor.clone();
+        newBox = new THREE.Box3Helper( newBoundingBox, 0xffff00 );
+    }
+}
 
+function get3DCoord() {
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.z = 0.5;
+    mouse.unproject( camera );
+    var dir = mouse.sub( camera.position ).normalize();
+    var distance = - camera.position.y / dir.y;
+    var pos = camera.position.clone().add( dir.multiplyScalar( distance ) );
+    return pos;
 }
 
 function onWindowResize() {
@@ -195,18 +231,17 @@ function render() {
 
     var intersections = raycaster.intersectObjects( [pointcloud] );
     intersection = ( intersections.length ) > 0 ? intersections[ 0 ] : null;
-
+    // if point is clicked on, color with red sphere
     if ( toggle > 0.005 && intersection !== null && !(
             pointsWithSpheres.has(intersection.index)) && mouseDown
             && !controls.enabled) {
-
         var point = pointcloud.geometry.vertices[intersection.index];
         var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
         sphere.position.copy( point );
         scene.add(sphere);
         spheres.push(sphere);
         pointsWithSpheres.add(intersection.index);
-
+        selectedPoints.push(point);
         toggle = 0;
     }
 
@@ -216,62 +251,93 @@ function render() {
 
 }
 
-// Navigation Bar
-
-function initNav() {
-    navigation = document.getElementById('navigation');
-    var i = 0;
-    for (key in data) {
-        var li = document.createElement('li');
-        var element = document.createElement('a');
-        li.appendChild(element);
-        element.innerHTML = i + 1;
-        element.id = 'obj' + i;
-        wrapper = function(key) { return function() { show(this, key); }}
-        element.onclick = wrapper(key);
-        navigation.appendChild(li);
-        i += 1;
-    }
-}
-
-function clearNav() {
-    for (var i = 0; i < Object.keys(data).length; i++) {
-        document.getElementById('obj' + i).className = '';
-    }
-}
-
-function show(button, obj_name) {
-    clearNav();
-    button.className += "selected";
+function show() {
     var rotation = 0;
 
     if (pointcloud !== undefined) {
         scene.remove(pointcloud);
         rotation = pointcloud.rotation.y;
     }
-
-    pointcloud = generatePointCloudForCluster( obj_name );
+    // add pointcloud to scene
+    pointcloud = generatePointCloudForCluster();
     pointcloud.rotation.y = rotation;
     scene.add( pointcloud );
 }
 
-function generatePointCloudForCluster(obj_name) {
-    return generatePointCloud(data[obj_name]['vertices'], new THREE.Color( 0,1,0 ));
+function generatePointCloudForCluster() {
+    return generatePointCloud(data, new THREE.Color( 0,1,0 ));
 }
 
 function moveMode( event ) {
+    console.log(camera.rotation);
     event.preventDefault();
     controls.enabled = true;
+    move2D = false;
     document.getElementById( 'label' ).className = "";
+    document.getElementById( 'move2D' ).className = "";
     document.getElementById( 'move' ).className = "selected";
+    controls.maxPolarAngle = 2 * Math.PI;
+    controls.minPolarAngle = -2 * Math.PI;
+}
+
+function move2DMode( event ) {
+    event.preventDefault();
+    controls.enabled = true;
+    controls.update();
+    document.getElementById( 'move' ).className = "";
+    document.getElementById( 'label' ).className = "";
+    document.getElementById( 'move2D' ).className = "selected";
+    if (!move2D) {
+        start = camera.rotation;
+        // end = dt * start.y;
+        // loop();
+        // renderer.render(scene, camera);
+        camera.rotation.y = 0;
+        controls.maxPolarAngle = 0;
+        controls.minPolarAngle = 0;
+        // camera.position.set(0, 100, 0);
+        // camera.lookAt(new THREE.Vector3(0,0,0));
+    }
+    move2D = true;
 }
 
 function labelMode( event ) {
     event.preventDefault();
-    controls.enabled = false;
-    document.getElementById( 'label' ).className = "selected";
-    document.getElementById( 'move' ).className = "";
+    if (move2D) {
+        controls.enabled = false;
+        controls.update();
+        document.getElementById( 'label' ).className = "selected";
+        document.getElementById( 'move' ).className = "";
+        document.getElementById( 'move2D' ).className = "";
+
+    }
 }
+
+// linear interpolation function
+function lerp(a, b, t) {return a + (b - a) * t}
+
+
+function loop() {
+    for (var i = 0; i < 100; i++) {
+        camera.rotation.y -= end;
+        controls.update();
+        // render();
+        // requestAnimationFrame(render);
+        // renderer.render(scene, camera);
+        console.log("loop " + i);
+    }
+  // var newX = lerp(a.x, b.x, ease(t));   // interpolate between a and b where
+  // var newY = lerp(a.y, b.y, ease(t));   // t is first passed through a easing
+  // var newZ = lerp(a.z, b.z, ease(t));   // function in this example.
+  // mesh.position.set(newX, newY, newZ);  // set new position
+  // t += dt;
+  // if (t <= 0 || t >=1) dt = -dt;        // ping-pong for demo
+  
+  // t += dt;
+  // renderer.render(scene, camera);
+  // requestAnimationFrame(loop);
+}
+
 
 function save() {
     textContents = [];
@@ -292,6 +358,56 @@ function save_image() {
     renderer.domElement.toBlob(function (blob) {
         saveAs(blob, "image.png");
     });
+}
+
+function upload_file() {
+    var x = document.getElementById("file_input");
+    if (x.files.length > 0) {
+        var file = x.files[0];
+        load_text_file(file, import_annotations_from_bin);
+    }
+}
+
+function import_annotations_from_bin(data) {
+  if ( data === '' || typeof(data) === 'undefined') {
+    return;
+  }
+}
+
+
+function load_text_file(text_file, callback_function) {
+  if (text_file) {
+    var text_reader = new FileReader();
+    text_reader.readAsArrayBuffer(text_file);
+    text_reader.onload = readData;
+    image_loaded = true;
+  }
+}
+
+function readData(e) {
+    var rawLog = this.result;
+    // console.log(rawLog);
+    var floatarr = new Float32Array(rawLog)
+    
+    data = floatarr;
+    // console.log(data);
+    // console.log(data.length);
+
+    show();
+    animate();
+}
+
+
+function drawBox() {    
+    var boundingBox = new THREE.Box3();
+    boundingBox.setFromPoints(selectedPoints);
+    console.log(boundingBox);
+    console.log(selectedPoints);
+    var helper = new THREE.Box3Helper( boundingBox, 0xffff00 );
+    boundingBoxes.push(boundingBox);
+    boxMap.set(boundingBox, selectedPoints);
+    selectedPoints = [];
+    scene.add( helper );
 }
 
 // https://stackoverflow.com/a/15327425/4855984
