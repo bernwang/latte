a4f84075bead184169612c6014234b747cc16d86
bernie wang <berniewang@berkeley.edu>
2018-05-05 12:25:57 -0700

    removed parser from git repo

--- parser/parser.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-
-
-e = ET.parse('/Users/berniewang/annotator/lidarAnnotator/test/' \
-								'velodyne_points/tracklet_labels/' \
-								'tracklet_labels_13.xml').getroot()
-
-for child in e:
-	print(child.tag, child.attrib)
